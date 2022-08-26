5026b65a0d85c8d5f79350adf3a00e18e231b9ca
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-08-26 10:17:39 -0500

    Creado controlador principal

---
+++ src/main/java/com/consultorioonline/controladores/ControladorPrincipal.java
@@ -0,0 +1,18 @@
+package com.consultorioonline.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorPrincipal {
+    
+    @GetMapping("/")
+    public String cargarIndex() {
+        return "index";
+    }
+    
+    @GetMapping("/login")
+    public String iniciarSesion() {
+        return "login";
+    }
+}
